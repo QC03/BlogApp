bc8cd87006229bac5ac2e1f3a9ecdc78107f09d6
QC03 <tjdwns5869@naver.com>
2025-08-25 17:38:09 +0900

    Docs: Edit Git Ignore

---
+++ backend/src/main/java/com/github/QC03/BlogSite/BlogSiteApplication.java
@@ -0,0 +1,15 @@
+package com.github.QC03.BlogSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSiteApplication.class, args);
+	}
+
+}
+
+// todo 이후 계회 -> 회원관리 기능 추가 (User, Login, Call API using Token)
